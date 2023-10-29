fd8da8302d6df4cfcba5e55ec3ffc4a87a5a08cd
pvmavzovin <pvmavzovin@edu.hse.ru>
2023-10-29 18:03:45 +0300

    add discriminant calculating, pmf laplace, pdf bernoulli plot, edf

--- bernoulli.py
+++ bernoulli.py
@@ -2,24 +2,8 @@
 from scipy.stats import bernoulli
 import matplotlib.pyplot as plt
 from random import random
-# Instance of Bernoulli distribution with parameter theta
-# theta = 0.8
-# bd=bernoulli(theta)
-#
-# # Outcome of random variable either 0 or 1
-# x=[0,1]
-#
-# # For the visualization of the bar plot of Bernoulli's distribution
-# plt.figure(figsize=(10,10))
-# plt.xlim(-2, 2)
-# plt.bar(x, bd.pmf(x), color='blue')
-#
-# # For labelling of Bar plot
-# plt.title('Bernoulli distribution (p=0.8)', fontsize='20')
-# plt.xlabel('Values of random variable x (0, 1)', fontsize='20')
-# plt.ylabel('Probability', fontsize='20')
-#
-# plt.show()
+import numpy as np
+
 
 def generate_bernoulli(theta, size):
     bd = bernoulli(theta)
@@ -33,32 +17,104 @@ def generate_bernoulli(theta, size):
         else:
             ans.append(0)
     return ans
-# Объемы выборок
+
+
 sample_sizes = [5, 10, 100, 200, 400, 600, 800, 1000]
 samples_bernoulli = dict()
 samples_laplace = dict()
-theta = 0.8
-size = 10
-print(generate_bernoulli(theta, size))
-# Генерация выборок
-for n in sample_sizes:
-    sample_bernoulli = generate_bernoulli(theta, n)
-    print(n)
-    print(sample_bernoulli)
-    samples_bernoulli[n] = sample_bernoulli
-    # sample_ray = generate_rayleigh(param_rayleigh, n)
-    # samples_ray[n] = sample_ray
-    # print(sample_ray)
-    # (Опционально) Выводим информацию о выборке
-    print(f"Generated {n} samples for Bernoulli and Laplace distributions.")
+theta_bernoulli = 0.8
+theta_laplace = 2.0  # The scale parameter of the Laplace distribution
+
+
+def generate_laplace_sample(theta_bernoulli, size):
+    # Generate Laplace-distributed samples
+    samples = np.random.laplace(scale=theta_bernoulli, size=size)
+    return samples.tolist()
+
+
+for sample_size in sample_sizes:
+    sample_bernoulli = generate_bernoulli(theta_bernoulli, sample_size)
+
+    samples_bernoulli[sample_size] = sample_bernoulli
+    print(f"Generated {sample_size} samples for Bernoulli and Laplace distributions.")
     plt.figure(figsize=(12, 6))
 
     plt.subplot(1, 2, 1)
     plt.hist(sample_bernoulli, bins='auto')
-    plt.title(f'Bernoulli distribution with {n} samples')
+    # plt.hist(sample_bernoulli, bins=30, density=True, alpha=0.6, color='b', label='Bernoulli Samples')
 
-    # plt.subplot(1, 2, 2)
-    # plt.hist(sample_ray, bins='auto')
-    # plt.title(f'Rayleigh distribution with {n} samples')
+    plt.title(f'Bernoulli Distribution (theta={theta_bernoulli}, sample_size={sample_size})')
+    laplace_sample = generate_laplace_sample(theta_laplace, sample_size)
+    samples_laplace[sample_size] = laplace_sample
+    plt.show()
+    plt.subplot(1, 2, 1)
+    plt.hist(laplace_sample, bins='auto')
+    # plt.hist(laplace_sample, bins=30, density=True, alpha=0.6, color='b', label='Laplace Samples')
 
+    plt.title(f'Laplace Distribution (theta={theta_laplace}, sample_size={sample_size})')
+    # plt.grid(True)
+
+    plt.show()
+
+# Эмпирическая функция распределения
+def empirical_cdf(sample, t):
+    return sum([1 for x in sample if x <= t]) / len(sample)
+
+# Вычисление Дискриминанта
+def compute_D(samples_x, samples_y):
+    combined_samples = sorted(set(samples_x + samples_y))
+    max_diff = 0
+    for t in combined_samples:
+        diff = abs(empirical_cdf(samples_x, t) - empirical_cdf(samples_y, t))
+        max_diff = max(max_diff, diff)
+    n, m = len(samples_x), len(samples_y)
+    D = np.sqrt(n * m / (n + m)) * max_diff
+    return D
+
+sample_sizes = [5, 10, 100, 200, 400, 600, 800, 1000]
+
+# Построение эмпирических функций распределения
+for size in sample_sizes:
+    t_vals = sorted(set(samples_bernoulli[size] + samples_laplace[size]))
+    bernoulli_cdf_vals = [empirical_cdf(samples_bernoulli[size], t) for t in t_vals]
+    laplace_cdf_vals = [empirical_cdf(samples_laplace[size], t) for t in t_vals]
+
+    plt.plot(t_vals, bernoulli_cdf_vals, label=f'Bernoulli (n={size})')
+    plt.plot(t_vals, laplace_cdf_vals, label=f'Laplace (n={size})')
+    plt.title(f'Empirical CDFs for n = {size}')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+# Вычисление D для каждой пары объемов выборок
+for n in sample_sizes:
+    for m in sample_sizes:
+        D = compute_D(samples_bernoulli[n], samples_laplace[m])
+        print(f'D for Bernoulli(n={n}) and Laplace(m={m}): {D}')
+
+
+def bernoulli_pdf(x, sigma):
+    return x / sigma**2 * np.exp(-x**2 / (2 * sigma**2))
+
+
+def laplace_pmf(k, p):
+    return -p**k / (k * np.log(1-p))
+
+for n in sample_sizes:
+    plt.figure(figsize=(12, 6))
+    plt.hist(np.array(samples_laplace[n]), bins=np.arange(1, max(samples_laplace[n]) + 2) - 0.5, density=True, rwidth=0.8, align='mid', label='Histogram')
+    k_vals = np.arange(1, max(samples_laplace[n]) + 1)
+    plt.plot(k_vals, [laplace_pmf(k, 0.5) for k in k_vals], 'o-', label='PMF')  # предполагаем, что p=0.5
+    plt.title(f'Laplace Distribution (n={n})')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+    plt.figure(figsize=(12, 6))
+    plt.hist(samples_bernoulli[n], bins=30, density=True, label='Histogram', alpha=0.7)
+    x_vals = np.linspace(0, max(samples_bernoulli[n]), 400)
+    plt.plot(x_vals, bernoulli_pdf(x_vals, 1), label='PDF')  # предполагаем, что sigma=1
+    plt.title(f'Bernoulli Distribution (n={n})')
+    plt.legend()
+    plt.grid(True)
     plt.show()
